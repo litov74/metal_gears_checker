7bfdee1ffe27815219ba9939fc0ad669e4b6906d
litov74 <levt382@gmail.com>
2021-08-25 17:04:35 +0500

    added data output

--- tools/data_generator.py
+++ tools/data_generator.py
@@ -102,53 +102,57 @@ def mask2pad(mask, pad=2):
 
     return mask
 
-plt.figure(figsize=(13.5,2.5))
-bar = plt.bar( [1,2,3,4],100*np.mean( train2.iloc[:,1:5]!='',axis=0) )
+
+plt.figure(figsize=(13.5, 2.5))
+bar = plt.bar([1, 2, 3, 4], 100 * np.mean(train2.iloc[:, 1:5] != '', axis=0))
 plt.title('Percent Training Images with Defect', fontsize=16)
-plt.ylabel('Percent of Images'); plt.xlabel('Defect Type')
-plt.xticks([1,2,3,4])
+plt.ylabel('Percent of Images');
+plt.xlabel('Defect Type')
+plt.xticks([1, 2, 3, 4])
 for rect in bar:
     height = rect.get_height()
-    plt.text(rect.get_x() + rect.get_width()/2.0, height, '%.1f %%' % height,
-             ha='center', va='bottom',fontsize=16)
-plt.ylim((0,50)); plt.show()
+    plt.text(rect.get_x() + rect.get_width() / 2.0, height, '%.1f %%' % height,
+             ha='center', va='bottom', fontsize=16)
+plt.ylim((0, 50));
+plt.show()
 
 filenames = {}
-defects = list(train2[train2['e1']!=''].sample(3).index)
-defects += list(train2[train2['e2']!=''].sample(3).index)
-defects += list(train2[train2['e3']!=''].sample(7).index)
-defects += list(train2[train2['e4']!=''].sample(3).index)
+defects = list(train2[train2['e1'] != ''].sample(3).index)
+defects += list(train2[train2['e2'] != ''].sample(3).index)
+defects += list(train2[train2['e3'] != ''].sample(7).index)
+defects += list(train2[train2['e4'] != ''].sample(3).index)
 
 # DATA GENERATOR
-train_batches = DataGenerator(train2[train2.index.isin(defects)],shuffle=True,info=filenames)
+train_batches = DataGenerator(train2[train2.index.isin(defects)], shuffle=True, info=filenames)
 print('Images and masks from our Data Generator')
 print('KEY: yellow=defect1, green=defect2, blue=defect3, magenta=defect4')
 
 # DISPLAY IMAGES WITH DEFECTS
-for i,batch in enumerate(train_batches):
-    plt.figure(figsize=(14,50)) #20,18
+for i, batch in enumerate(train_batches):
+    plt.figure(figsize=(14, 50))  # 20,18
     for k in range(16):
-        plt.subplot(16,1,k+1)
+        plt.subplot(16, 1, k + 1)
         img = batch[0][k,]
         img = Image.fromarray(img.astype('uint8'))
         img = np.array(img)
         extra = '  has defect'
         for j in range(4):
-            msk = batch[1][k,:,:,j]
-            msk = mask2pad(msk,pad=3)
-            msk = mask2contour(msk,width=2)
-            if np.sum(msk)!=0: extra += ' '+str(j+1)
-            if j==0: # yellow
-                img[msk==1,0] = 235
-                img[msk==1,1] = 235
-            elif j==1: img[msk==1,1] = 210 # green
-            elif j==2: img[msk==1,2] = 255 # blue
-            elif j==3: # magenta
-                img[msk==1,0] = 255
-                img[msk==1,2] = 255
-        plt.title(filenames[16*i+k]+extra)
+            msk = batch[1][k, :, :, j]
+            msk = mask2pad(msk, pad=3)
+            msk = mask2contour(msk, width=2)
+            if np.sum(msk) != 0: extra += ' ' + str(j + 1)
+            if j == 0:  # yellow
+                img[msk == 1, 0] = 235
+                img[msk == 1, 1] = 235
+            elif j == 1:
+                img[msk == 1, 1] = 210  # green
+            elif j == 2:
+                img[msk == 1, 2] = 255  # blue
+            elif j == 3:  # magenta
+                img[msk == 1, 0] = 255
+                img[msk == 1, 2] = 255
+        plt.title(filenames[16 * i + k] + extra)
         plt.axis('off')
         plt.imshow(img)
     plt.subplots_adjust(wspace=0.05)
     plt.show()
-
